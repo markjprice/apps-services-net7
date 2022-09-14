2bf429dcca1d85a57b9a39815ed24ae0d6b37795
Mark J Price <markjprice@gmail.com>
2022-09-14 13:09:13 +0100

    Initial commit

---
+++ vscode/Scripts/install-vs-code-extensions.ps1
@@ -0,0 +1,10 @@
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.dotnet-interactive-vscode
+code --install-extension tintoy.msbuild-project-tools
+code --install-extension humao.rest-client
+code --install-extension icsharpcode.ilspy-vscode
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension github.remotehub
+code --install-extension ms-mssql.mssql
+code --install-extension zxh404.vscode-proto3
+
