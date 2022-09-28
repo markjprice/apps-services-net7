fc49ad6e858d84b01cca30c56238276a61031281
Mark J Price <markjprice@gmail.com>
2022-09-28 13:31:03 +0100

    Initial commit

--- vs4win/Chapter13/Northwind.SignalR.Client.Console.Streams/Program.Methods.cs
+++ vs4win/Chapter13/Northwind.SignalR.Client.Console.Streams/Program.Methods.cs
@@ -4,7 +4,7 @@
   {
     for (int i = 0; i < 10; i++)
     {
-      // Return a random four letter stock code.
+      // Return a random four-letter stock code.
       yield return $"{AtoZ()}{AtoZ()}{AtoZ()}{AtoZ()}";
 
       await Task.Delay(3000); // milliseconds
